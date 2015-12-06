f1e0f1c8e416aa0c4b1e46dfef640dc398d69841
Brian Wong <brianwong1222@hotmail.com>
2015-12-06 03:00:09 -0600

    a letter changed

--- app/src/main/java/team10/studybuddy/Settings.java
+++ app/src/main/java/team10/studybuddy/Settings.java
@@ -22,7 +22,7 @@ public class Settings extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_settings);
 
         fname = (CheckBox) findViewById(R.id.setting_firstname);
         lname = (CheckBox) findViewById(R.id.setting_lastname);
