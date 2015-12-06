3a01c5c40a026d88974323f78f482411172677d4
Brian Wong <brianwong1222@hotmail.com>
2015-12-06 02:14:51 -0600

    StudyGroup class created
    StudyGroup class created with pseudocode of an idea of how to implement.
    Nothing implemented.

---
+++ app/src/main/java/team10/studybuddy/StudyGroup.java
@@ -0,0 +1,26 @@
+package team10.studybuddy;
+
+/**
+ * Created by Brian on 12/6/2015.
+ */
+public class StudyGroup {
+
+    /**
+     *  Pseudocode for StudyGroup class
+     *
+     *  Creating group
+     *      Users can search other users taking the same class
+     *      User adds other user to group
+     *      Group array created containing name and userID
+     *      Store group array into parse.com and return groupID
+     *
+     *  Retrieving group
+     *      Search for array using groupID
+     *      Allow members in group array to rate other members for performance
+     *      Each user is updated with a new average rating and no_of_ratings is incremented for each rating added
+     *      Remove group array from parse.com
+     *      Store user updated data into parse.com
+     *
+     */
+
+}
